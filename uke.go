d5878b069c9dfce66d5623d220d46258c529e3c7
Michael Klein <m5l.k3n@gmail.com>
2022-02-16 23:56:14 +0100

    basic chords done

--- uke.go
+++ uke.go
@@ -17,6 +17,30 @@ type Chord struct {
 	singleChords []SingleChord
 }
 
+// finger labels for key
+const indexFinger = "1"
+const middleFinger = "2"
+const ringFinger = "3"
+const pinky = "4"
+
+// basic chords
+var C = Chord{"C", []SingleChord{{3, 2, ringFinger}}}
+var Am = Chord{"Am", []SingleChord{{0, 1, middleFinger}}}
+var F = Chord{"F", []SingleChord{{2, 0, indexFinger}, {0, 1, middleFinger}}}
+var G = Chord{"G", []SingleChord{{1, 1, indexFinger}, {3, 1, middleFinger}, {2, 2, ringFinger}}}
+var A = Chord{"A", []SingleChord{{1, 0, indexFinger}, {0, 1, middleFinger}}}
+var Em = Chord{"Em", []SingleChord{{3, 1, indexFinger}, {2, 2, middleFinger}, {1, 3, ringFinger}}}
+var D = Chord{"D", []SingleChord{{0, 1, indexFinger}, {1, 1, middleFinger}, {2, 1, ringFinger}}}
+var Dm = Chord{"Dm", []SingleChord{{2, 0, indexFinger}, {0, 1, middleFinger}, {1, 1, ringFinger}}}
+var E = Chord{"E", []SingleChord{{0, 0, indexFinger}, {3, 1, middleFinger}, {1, 3, pinky}}}
+var Gbm = Chord{"F#/Gbm", []SingleChord{{1, 0, indexFinger}, {0, 1, middleFinger}, {2, 1, ringFinger}}}
+var Bm = Chord{"Bm", []SingleChord{{1, 1, indexFinger}, {2, 1, indexFinger}, {3, 1, indexFinger}, {0, 3, ringFinger}}}
+var Cm = Chord{"Cm", []SingleChord{{1, 2, indexFinger}, {2, 2, indexFinger}, {3, 2, indexFinger}}}
+var Gm = Chord{"Gm", []SingleChord{{3, 0, indexFinger}, {1, 1, middleFinger}, {2, 2, ringFinger}}}
+var G7 = Chord{"G7", []SingleChord{{2, 0, indexFinger}, {1, 1, middleFinger}, {3, 1, ringFinger}}}
+var C7 = Chord{"C7", []SingleChord{{3, 0, indexFinger}}}
+var A7 = Chord{"A7", []SingleChord{{1, 0, indexFinger}}}
+
 type fretboardMatrix [9]string
 
 var blankFretboard fretboardMatrix = fretboardMatrix{
@@ -50,20 +74,27 @@ func (f Fretboard) printFingers(c Chord, printKey bool) {
 		fmt.Println(f.fretboard[i])
 	}
 	if printKey {
-		fmt.Println("1 = index finger, 2 = middle finger, 3 = ring finger, 4 = pinky")
+		fmt.Printf("%s = index finger, %s = middle finger, %s = ring finger, %s = pinky\n", indexFinger, middleFinger, ringFinger, pinky)
 	}
 	fmt.Println()
 }
 
 func main() {
-	C := Chord{"C", []SingleChord{{3, 2, "3"}}}
-	Am := Chord{"Am", []SingleChord{{0, 1, "2"}}}
-	F := Chord{"F", []SingleChord{{2, 0, "1"}, {0, 1, "2"}}}
-	G := Chord{"G", []SingleChord{{1, 1, "1"}, {3, 1, "2"}, {2, 2, "3"}}}
-
 	f := Fretboard{blankFretboard}
 	f.printFingers(C, true)
 	f.printFingers(Am, false)
 	f.printFingers(F, false)
 	f.printFingers(G, false)
+	f.printFingers(A, false)
+	f.printFingers(Em, false)
+	f.printFingers(D, false)
+	f.printFingers(Dm, false)
+	f.printFingers(E, false)
+	f.printFingers(Gbm, false)
+	f.printFingers(Bm, false)
+	f.printFingers(Cm, false)
+	f.printFingers(Gm, false)
+	f.printFingers(G7, false)
+	f.printFingers(C7, false)
+	f.printFingers(A7, false)
 }
